50fc87bd40dbb727afcc5ab08ebb10f3d5b54eda
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-19 11:40:15 +0900

    [Add] 1つしかメニューを開けない

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@
   dts.forEach((dt) => {
     dt.addEventListener("click", () => {
       dt.parentNode.classList.toggle("appear");
+      dts.forEach((el) => {
+        if (dt !== el) {
+          el.parentNode.classList.remove("appear");
+        }
+      });
     });
   });
 }
